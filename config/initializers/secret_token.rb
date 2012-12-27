4f3e4f624b6d7d31beacbdff138a15b2a8d0b995
Michael Joseph Kramer <michael.kramer@live.com>
2012-12-27 08:48:08 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codemashlore::Application.config.secret_token = 'b142a7f1d4908f64c74db1f0b17a37a7f183de74d5b8c50374666833c7c8564c2fbc5470a38486086e166386eef1eaaee2a15039e7ac143f51647a1d22ba453c'
