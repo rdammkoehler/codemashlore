1b081f4f0898f9e57d57d6ff43fa73bd81029d26
Tim Wingfield <tim@timwingfield.com>
2013-01-08 11:46:28 -0500

    updated intro lab instructions

--- app/assets/javascripts/intro.js
+++ app/assets/javascripts/intro.js
@@ -21,12 +21,12 @@ Intro.theModel = Backbone.Model.extend({
 /*Lab: Create a simple calculator to operate on two numbers
  * Model
  *  1. Should contain the operands
- *  2. Should contain the arithmetic
+ *  2. Should contain the arthmetic functions
  *  
  * View
  *  1. Take a model at initialize
  *  2. Create a simple render function
  *  3. Bind to any model changes
- *  4. Display the sum, difference, product, and quotient
- *  4. Bonus points: Have it add the numbers via model function rather than model bind
+ *  4. Output the sum, difference, product, and quotient of the operands
+ *  5. Bonus points: Set up a simple form and button to take in the operands
 */ 
