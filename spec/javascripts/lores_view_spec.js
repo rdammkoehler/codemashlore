5c28b8d1a11f4bdbefacb8c6742000fdb6e1f781
Michael Joseph Kramer <michael.kramer@live.com>
2013-01-08 11:39:28 -0500

    Minor syntax cleanup

--- spec/javascripts/lores_view_spec.js
+++ spec/javascripts/lores_view_spec.js
@@ -1,4 +1,5 @@
 describe('lores view', function() {
+
   beforeEach(function() {
     setFixtures('<div id=\'lores\'></div>');
     this.collection = new CodemashLore.Collections.Lores([
@@ -12,23 +13,27 @@ describe('lores view', function() {
         ranking: 1
       }
     ]);
-    return this.subject = new CodemashLore.Views.Lores({
+    this.subject = new CodemashLore.Views.Lores({
       collection: this.collection,
       el: '#lores'
     });
   });
+
   it('has a template', function() {
-    return expect(this.subject.template).toBeDefined();
+    expect(this.subject.template).toBeDefined();
   });
+
   it('has the correct events', function() {
-    return expect(this.subject.events['click .up-vote']).toEqual('upvote');
+    expect(this.subject.events['click .up-vote']).toEqual('upvote');
   });
+
   it('renders correctly', function() {
     spyOn(this.subject.$el, 'html');
     this.subject.render();
-    return expect(this.subject.$el.html).toHaveBeenCalled();
+    expect(this.subject.$el.html).toHaveBeenCalled();
   });
-  return it('responds to upvote correctly', function() {
+
+  it('responds to upvote correctly', function() {
     var e, lore;
     lore = this.collection.get(1);
     spyOn(lore, 'save');
@@ -37,6 +42,7 @@ describe('lores view', function() {
     };
     this.subject.upvote(e);
     expect(lore.get('ranking')).toEqual(2);
-    return expect(lore.save).toHaveBeenCalled();
+    expect(lore.save).toHaveBeenCalled();
   });
+
 });
